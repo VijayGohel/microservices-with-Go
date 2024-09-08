c2bf225c22bf92272924408fb7c4675cd25372f8
vijaygohel <vijaygohel5454@gmail.com>
2024-09-09 00:12:53 +0530

    connect to postgres

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "80"
+
+var dbRetriesCount int32
 
 type Config struct {
 	DB     *sql.DB
@@ -16,9 +20,19 @@ type Config struct {
 }
 
 func main() {
-	app := Config{}
+	log.Printf("Starting authentication service on port %s\n", webPort)
+	
+	//connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Fatal("Could not connect to Postgres")
+	}
+	defer conn.Close()
 
-	log.Printf("Starting broker service on port %s\n", webPort)
+	app := Config{
+		DB:     conn,
+        Models: data.New(conn),
+	}
 
 	//define http server
 	serv := &http.Server{
@@ -30,5 +44,43 @@ func main() {
 	err := serv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+        return nil, err
+    }
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		db, err := openDB(dsn)
+        if err != nil {
+			log.Println("Postgres not yet ready...")
+            dbRetriesCount++
+        } else {
+			log.Printf("Connected to Postgres after %d retries\n", dbRetriesCount)
+			return db
+		}
+
+		if dbRetriesCount >= 10 {
+            log.Fatal("Unable to connect to Postgres after 10 retries", err)
+			return nil;
+        }
+		
+        log.Println("Retrying connection to Postgres...")
+		time.Sleep(time.Second * 2)
+		continue
 	}
 }
